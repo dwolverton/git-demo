2b1372849f8f86fe7b5f5866ca6743ce8004d2b4
David Wolverton <dwolvert@gmail.com>
2023-01-04 20:56:34 -0500

    new name

--- src/gitdemo/MainApp.java
+++ src/gitdemo/MainApp.java
@@ -3,7 +3,7 @@ package gitdemo;
 public class MainApp {
 
 	public static void main(String[] args) {
-		String name = "Khalil";
+		String name = "Sara";
 		Other.yell("Hi " + name);
 		System.out.println("Goodbye " + name);
 	}
