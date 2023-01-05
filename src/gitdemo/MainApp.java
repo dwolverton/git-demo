3b2b72cc0c263df6f224b652009f5923ad53ecee
David Wolverton <dwolvert@gmail.com>
2023-01-04 21:07:12 -0500

    changed greeting

--- src/gitdemo/MainApp.java
+++ src/gitdemo/MainApp.java
@@ -4,7 +4,7 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		String name = "Sara";
-		Other.yell("Hi " + name);
+		Other.yell("Hello " + name);
 		System.out.println("Goodbye " + name);
 	}
 
