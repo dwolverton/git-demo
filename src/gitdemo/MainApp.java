b4a4492c214e85fd23b4763473c8f9f5ed184879
David Wolverton <dwolvert@gmail.com>
2023-01-04 21:05:21 -0500

    with an h

--- src/gitdemo/MainApp.java
+++ src/gitdemo/MainApp.java
@@ -3,7 +3,7 @@ package gitdemo;
 public class MainApp {
 
 	public static void main(String[] args) {
-		String name = "Sara";
+		String name = "Sarah";
 		Other.yell("Hi " + name);
 		System.out.println("Goodbye " + name);
 	}
